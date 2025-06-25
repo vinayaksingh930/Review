17aae842939066c72326e674f92bc1ba0d226301
anuragyadav-bit <anurag051104@gmail.com>
2025-06-25 12:32:19 +0530

    [Anurag] Add the simple Sum of 2-D Array

---
+++ SumOfTwoDArray.java
@@ -0,0 +1,16 @@
+public class SumOfTwoDArray {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                sum+=arr[i][j];
+            }
+        }
+        System.out.print(sum);
+    }
+}
